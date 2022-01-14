a07d246437a288ceb8aa53e20bcab550391df31e
phamtanphat <phatdroid94@gmail.com>
2022-01-14 18:55:07 +0700

    re name

--- app/src/main/java/com/example/roomdatabase18102021/data/local/WorkDao.java
+++ app/src/main/java/com/example/roomdatabase18102021/data/local/WorkDao.java
@@ -1,7 +1,10 @@
 package com.example.roomdatabase18102021.data.local;
 
 import androidx.room.Dao;
+import androidx.room.Query;
 
 @Dao
 public interface WorkDao {
+
+    @Query("SELECT * FROM work")
 }
--- app/src/main/java/com/example/roomdatabase18102021/data/local/WorkEntity.java
+++ app/src/main/java/com/example/roomdatabase18102021/data/local/WorkEntity.java
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity
+@Entity(tableName = "work")
 public class WorkEntity {
 
     @PrimaryKey(autoGenerate = true)
